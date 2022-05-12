8a73e4d029ff1ef0bf83b8feea629624ff00fa2c
Romain Chardiny <romain.chardiny@gmail.com>
2022-05-12 23:40:19 +0200

    fixes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import mapgen
 
-generated_map = mapgen.AbstractMap(30, 30, 3)
+generated_map = mapgen.AbstractMap(30, 30, max_rooms=3)
 generated_map.random_map()
 generated_map.make_paths()
 generated_map.display()
+
+print(generated_map)
