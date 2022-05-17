7528e14c59ce2fc56940e7d7254f48a06691024e
Romain Chardiny <romain.chardiny@gmail.com>
2022-05-17 17:51:50 +0200

    many things

--- app.py
+++ app.py
@@ -2,67 +2,149 @@ import random
 import sys
 import pygame
 import mapgen
-
-m = mapgen.Map(30, 30, max_rooms=9)
-m.generate_random()
-camera_size = 12
+import itertools
 
 size = width, height = 900, 900
 black = 0, 0, 0
 white = 255, 255, 255
 
-dpi = width / camera_size
+SCREENRECT = pygame.Rect(0, 0, width, height)
+
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
 
-wall = pygame.image.load("wall.png")
-wall = pygame.transform.scale(wall, (dpi, dpi))
+camera_size = 12
+camera_x, camera_y = 0, 0
 
-floor1 = pygame.image.load("floor1.png")
-floor1 = pygame.transform.scale(floor1, (dpi, dpi))
+dpi = width / camera_size
 
-floor2 = pygame.image.load("floor2.png")
-floor2 = pygame.transform.scale(floor2, (dpi, dpi))
+def loadify(path, size=0):
+    global dpi
+    return pygame.transform.scale(pygame.image.load(path), (dpi - size, dpi - size)).convert()
 
-pos_x, pos_y = 0, 0
+def inverse_direction(direction):
+    return (-direction[0], -direction[1])
 
-def clamp_pos():
-    global pos_x, pos_y
-    if pos_x < 0:
-        pos_x = 0
-    if pos_y < 0:
-        pos_y = 0
+def translated_rect(rect):
+    global camera_x, camera_y
+    return pygame.Rect(rect.x - camera_x, rect.y - camera_y, rect.width, rect.height)
+
+def check_adjacent(x, y, grid):
+    for dx, dy in itertools.product((-1, 0, 1), (-1, 0, 1)):
+        if (dx == 0 and dy == 0) or (x == 0 and dx == -1) or (y == 0 and dy == -1) or (x == len(grid[0]) - 1 and dx == 1) or (y == len(grid) - 1 and dy == 1):
+            continue
+        if grid[y + dy][x + dx] != ".":
+            return True
+    return False
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, initial_position=None):
+        super().__init__(self.containers)
+        self.origin_rect = self.image.get_rect()
+        if initial_position is None:
+            initial_position = (0, 0)
+        (self.origin_rect.x, self.origin_rect.y) = initial_position
+
+    @property
+    def rect(self):
+        return translated_rect(self.origin_rect)
+
+    def draw(self, screen):
+        screen.blit(self.image, translated_rect(self.origin_rect))
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, initial_position=None):
+        super().__init__(self.containers)
+        self.origin_rect = self.image.get_rect()
+        if initial_position is None:
+            initial_position = (0, 0)
+        (self.origin_rect.x, self.origin_rect.y) = initial_position
+
+    @property
+    def rect(self):
+        return translated_rect(self.origin_rect)
+
+    def draw(self, screen):
+        screen.blit(self.image, translated_rect(self.origin_rect))
+
+class Player(pygame.sprite.Sprite):
+    speed = 10
+
+    def __init__(self):
+        super().__init__(self.containers)
+        self.origin_rect = self.image.get_rect(center=SCREENRECT.center)
+
+    def move(self, direction):
+        global camera_x, camera_y
+        camera_x += direction[0]
+        camera_y += direction[1]
+        self.origin_rect.move_ip(direction)
+        if any(pygame.sprite.spritecollide(self, obstacle_group, False)):
+            self.origin_rect.move_ip(inverse_direction(direction))
+            camera_x -= direction[0]
+            camera_y -= direction[1]
+
+    @property
+    def rect(self):
+        return translated_rect(self.origin_rect)
+
+    def draw(self, screen):
+        screen.blit(self.image, translated_rect(self.origin_rect))
+
+abstract_map = mapgen.Map(40, 40)
+abstract_map.generate_random()
+
+abstract_map.display()
+
+background = pygame.Surface(size)
+pygame.display.flip()
+
+all_sprites = pygame.sprite.OrderedUpdates()
+obstacle_group = pygame.sprite.Group()
+
+Player.containers = all_sprites
+Floor.containers = all_sprites
+Wall.containers = all_sprites#, obstacle_group
+
+Player.image = loadify("player.png", size=3)
+Wall.image = loadify("wall.png")
+Floor.image = loadify("floor1.png")
+
+map_grid = abstract_map.grid()
+for y, row in enumerate(map_grid):
+    for x, elem in enumerate(row):
+        if elem in ('%', '#'):
+            Floor((x * dpi, y * dpi))
+        elif elem == '.':
+            if check_adjacent(x, y, map_grid):
+                Wall((x * dpi, y * dpi))
+            
+player = Player()
 
 while True:
+    all_sprites.clear(screen, background)
+
+    all_sprites.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_z:
-                pos_y -= 1
-                clamp_pos()
-            if event.key == pygame.K_q:
-                pos_x -= 1
-                clamp_pos()
-            if event.key == pygame.K_s:
-                pos_y += 1
-                clamp_pos()
-            if event.key == pygame.K_d:
-                pos_x += 1
-                clamp_pos()
 
-    screen.fill(black)
-    view = m.slice(mapgen.Coord(pos_x, pos_y), camera_size + 1, camera_size + 1)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        direction = (0, -1)
+        player.move(direction)
+    if keys[pygame.K_q]:
+        direction = (-1, 0)
+        player.move(direction)
+    if keys[pygame.K_s]:
+        direction = (0, 1)
+        player.move(direction)
+    if keys[pygame.K_d]:
+        direction = (1, 0)
+        player.move(direction)
 
-    for y, row in enumerate(view):
-        for x, elem in enumerate(row):
-            to_draw = None
-            is_color = False
-            if elem == '.':
-                to_draw = wall
-            else:
-                to_draw = floor1
-            if is_color:
-                screen.fill(to_draw, pygame.Rect(x * dpi, y * dpi, dpi, dpi))
-            else:
-                screen.blit(to_draw, pygame.Rect(x * dpi, y * dpi, dpi, dpi))
+    dirty = all_sprites.draw(screen)
+    
+    pygame.display.update(dirty)
 
-    pygame.display.flip()
+    clock.tick(240)
